c28f9f4927c75ef36b4e7205713543bc2c94e765
Carl <shikang.liu@mail.mcgill.ca>
2017-03-28 21:24:56 -0400

    add chat panel and chat feature

---
+++ Assets/Scripts/Catan/GamePieces/Die/DiePhysics.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiePhysics : MonoBehaviour {
+	public float forceAmount = 10.0f;
+	public float torqueAmount = 10.0f;
+	public ForceMode forceMode;
+
+	void Start(){
+	}
+
+	public void init(Vector3 randomVector){
+		GetComponent<Rigidbody>().AddForce (randomVector * forceAmount, forceMode);
+		GetComponent<Rigidbody>().AddTorque (randomVector * torqueAmount, forceMode);
+	}
+//	//Update is called once per frame
+//	void Update(){
+//		if (Input.GetKeyDown (KeyCode.Space)) {
+//			GetComponent<Rigidbody>().AddForce (Random.onUnitSphere * forceAmount, forceMode);
+//			GetComponent<Rigidbody>().AddTorque (Random.onUnitSphere * torqueAmount, forceMode);
+//		}
+//	}
+}
---
+++ Assets/Scripts/Catan/GamePieces/Die/FaceDetection.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FaceDetection : MonoBehaviour {
+
+	public List<Vector3> directions;
+	public List<int> sideValues;
+
+	void Awake() {
+		if (directions.Count == 0) {
+			// Pre-defined direction and reference values
+			directions.Add(Vector3.up);
+			sideValues.Add(5); // up
+			directions.Add(Vector3.down);
+			sideValues.Add(2); // down
+
+			directions.Add(Vector3.left);
+			sideValues.Add(3); // left
+			directions.Add(Vector3.right);
+			sideValues.Add(4); // right
+
+			directions.Add(Vector3.forward);
+			sideValues.Add (1); // fw
+			directions.Add(Vector3.back);
+			sideValues.Add(6); // back
+		}
+
+		if (directions.Count != sideValues.Count) {
+			Debug.LogError("Not consistent list sizes");
+		}
+	}
+
+	public void showNumber() {
+		string name = transform.name;
+		Debug.Log("The "+name+" has value: " + GetNumber(Vector3.up, 30f));
+	}
+
+	// Gets the number of the side pointing in the same direction as the reference vector,
+	// allowing epsilon degrees error.
+	public int GetNumber(Vector3 referenceVectorUp, float epsilonDeg = 5f) {
+		// here I would assert lookup is not empty, epsilon is positive and larger than smallest possible float etc
+		// Transform reference up to object space
+		Vector3 referenceObjectSpace = transform.InverseTransformDirection(referenceVectorUp);
+
+		// Find smallest difference to object space direction
+		float min = float.MaxValue;
+		int mostSimilarDirectionIndex = -1;
+		for (int i=0; i < directions.Count; ++i) {
+			float a = Vector3.Angle(referenceObjectSpace, directions[i]);
+			if (a <= epsilonDeg && a < min) {
+				min = a;
+				mostSimilarDirectionIndex = i;
+			}
+		}
+
+		// -1 as error code for not within bounds
+		return (mostSimilarDirectionIndex >= 0) ? sideValues[mostSimilarDirectionIndex] : -1; 
+	}
+}
