ddc804046439a0524db0f7f28b1fc88073811c3b
Nehirakdag <nehirakdag34@hotmail.com>
2017-04-06 00:00:35 -0400

    Extra commit for the UI files

---
+++ Assets/Scripts/Other/ListShuffler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+// Static class used to randomly shuffle a list.
+// Random Selector nodes (decorator) of the behavior
+// tree make use of this utility class to
+// randomly iterate through its children
+public static class ListShuffler {
+
+	public static void Shuffle<T>(this IList<T> list) {  
+		int n = list.Count;  
+		while (n > 1) {  
+			n--;  
+			int k = Random.Range (0, n + 1);
+			T value = list[k];  
+			list[k] = list[n];  
+			list[n] = value;  
+		}  
+	}
+
+}
